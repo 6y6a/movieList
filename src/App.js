005b23e246777172cf7923983859ae42b76fbe79
6y6a <gubinalexeymain@gmail.com>
2019-09-28 03:10:04 +0300

    добавлен роутинг

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React from 'react';
 import './App.css';
 import 'font-awesome/css/font-awesome.min.css'
 import Movies from "./components/Movies";
+import Navbar from "./components/Navbar";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import PageMovies from "./pages/PageMovies";
+import PageCustomers from "./pages/PageCustomers";
+import PageRentals from "./pages/PageRentals";
+import PageNotFound from "./pages/PageNotFound";
+import PageMovie from "./pages/PageMovie";
 
 function App() {
-  return (
-    <main className='container'>
-      <Movies/>
-    </main>
-  );
+    return (
+        <main>
+            <Navbar/>
+
+            <div className="container mt-4">
+                <Switch>
+                    <Route path='/movies/:id' component={PageMovie}/>
+                    <Route path='/movies' component={PageMovies}/>
+                    <Route path='/customers' component={PageCustomers}/>
+                    <Route path='/rentals' component={PageRentals}/>
+                    <Route path='/not-found' component={PageNotFound}/>
+                    <Redirect exact from='/' to='/movies'/>
+                    <Redirect to='/not-found'/>
+                </Switch>
+            </div>
+
+        </main>
+    );
 }
 
 export default App;
