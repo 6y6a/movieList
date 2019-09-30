f5fd3814f6b52bbe2ca55b518e252cb86fd51505
6y6a <gubinalexeymain@gmail.com>
2019-09-30 19:12:17 +0300

    добавлен компонент выбора

---
+++ src/components/Select.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+function Select ({label, name, options, error, ...rest}) {
+    return (
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <select className="form-control" id={name} name={name} {...rest}>
+                <option value='' />
+                {options.map(item => (
+                    <option key={item._id} value={item._id}>{item.name}</option>
+                ))}
+            </select>
+            { error && <div className='alert alert-danger'>{error}</div>}
+        </div>
+    )
+}
+
+export default Select;
