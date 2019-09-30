c1e05c1d65efc6b08acc0b12563fa9e0629259b6
6y6a <gubinalexeymain@gmail.com>
2019-09-30 19:13:43 +0300

    вывод селекта

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Joi from "joi-browser";
 import Input from "./Input";
+import Select from "./Select";
 
 class Form extends Component {
     state = {
@@ -72,6 +73,21 @@ class Form extends Component {
                 type={type}
                 name={name}
                 label={label}
+                value={data[name]}
+                error={errors[name]}
+                onChange={this.handleChange}
+            />
+        )
+    }
+
+    renderSelect(name, label, options) {
+        const {data, errors} = this.state
+
+        return (
+            <Select
+                name={name}
+                label={label}
+                options={options}
                 value={data[name]}
                 error={errors[name]}
                 onChange={this.handleChange}
